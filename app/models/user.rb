b0f1c3ae6ca9d2649447b198bf127616f37bbfb6
Matheus Koiti <matheus.koiti.ito@gmail.com>
2023-10-12 21:34:36 -0300

    - Changed the validation method so the arbitrarily chosen format is now only formatted if the original cpf or phone string are validated - Made corrections to some faulty logic in validation - After some testing certified that CPF and Phone only validates and saves if are unique and in a correct format - Fixed error where the CPF and PHONE strings were modified even when the format was incorrect.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,107 @@
 class User < ApplicationRecord
+    attr_accessor :cpf_input
+    
     validates :name, presence: true, uniqueness: true
     
     validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     
-    validate :phone_format
-    validates :phone, uniqueness: true
+    validates :phone, presence: true
+    validate :validate_format_and_uniqueness, if: -> { phone.present? }
     
-    validate :cpf_format
-    validates :cpf, uniqueness: true
-    validate :cpf_digits
+    validates :cpf, presence: true
+    validate :validate_format_and_uniqueness, if: -> { cpf.present? }
 
-    before_validation :ajustar_cpf
-    validates :cpf, uniqueness: true
- 
+
+    private
+
+    def validate_format_and_uniqueness
+        cpf_stripped = cpf.gsub(/\D/, '') # retira os caracteres nao-numericos da string do cpf
+        existing_user = User.find_by("cpf = :cpf OR cpf = :cpf_stripped", cpf: cpf, cpf_stripped: cpf_stripped)
     
-    before_validation :ajustar_phone
-    validates :phone, uniqueness: true
+        if valid_cpf_format?(cpf) && existing_user.nil?
+          self.cpf = cpf_stripped
+        elsif !valid_cpf_format?(cpf)
+          errors.add(:cpf, "is not a valid CPF")
+        else
+          errors.add(:cpf, "already exists in the database")
+        end
+    end
 
     
+    def valid_cpf_format?(cpf)
+        # Define the allowed CPF formats
+        allowed_cpf_formats = [
+          /\A\d{3}\.\d{3}\.\d{3}\-\d{2}\z/,       # formato : 000.000.000-00
+          /\A\d{11}\z/                            # formato : 00000000000
+        ]
+      
+        # Checa se o cpf fornecido coincide com algum dos formatos possíveis
+        valid_format = allowed_cpf_formats.any? { |format| format.match?(cpf) }
+      
+        # Se o formato é valido checa se o numero de cpf é válido 
+        if valid_format
+            cpf_clean = cpf.gsub(/\D/,'')
+            cpf_int = cpf_clean.to_i 
+            
+            soma = 0
+            for i in (10).downto(2)
+              temp = ((cpf_int/10**i)%10) * i
+              soma = soma + temp
+            end
+            
+            soma2 = 0
+            for j in (11).downto(2)
+              temp = ((cpf_int/10**(j-1))%10) * j
+              soma2 = soma2 + temp
+            end
+            
+            resto = soma % 11
+            if resto < 2
+                comp = 0
+            else
+                comp = 11 - resto
+            end
     
-    def phone_format
-        # Define um vetor de formatos de números permitidos
+            resto2 = soma2 % 11
+            
+            if resto2 < 2
+                comp2 = 0
+            else
+                comp2 = 11 - resto2 
+            end
+    
+            cpf_digito_um = (cpf_int/10)%10
+            cpf_digito_dois = cpf_int%10
+
+            valid_digits = (comp == cpf_digito_um && comp2 == cpf_digito_dois) && !all_digits_same?(cpf_clean)
+      
+            return valid_digits
+        end
+    end
+    
+    def validate_format_and_uniqueness
+        phone_cleaned = phone.gsub(/\D/, '') 
+        phone_cleaned = self.phone.gsub(/\D/,'')
+        if((phone_cleaned.length != 12) && (phone_cleaned.length != 13))
+            phone_cleaned = "+55" + phone_cleaned
+        else
+            phone_cleaned = "+" + phone_cleaned
+        end
+
+        existing_user = User.find_by("phone = :phone OR phone = :phone_cleaned", phone: phone, phone_cleaned: phone_cleaned)
+
+        if valid_phone_format?(phone) && existing_user.nil?
+          self.phone = phone_cleaned
+        elsif !valid_phone_format?(phone)
+          errors.add(:phone, "is not a valid phone number")
+        else
+          errors.add(:phone, "número de telefone já utilizado")
+        end
+    end  
+    
+    
+    
+    def valid_phone_format?(phone)
         allowed_phone_formats = [
             /\A\d{11}\z/,                    # Formato: 00000000000      >
             /\A\(\d{2}\)\d{5}\-\d{4}\z/,     # Formato: (00)00000-0000   > celulares
@@ -29,97 +110,14 @@ class User < ApplicationRecord
             /\A\(\d{2}\)\d{4}\-\d{4}\z/,     # Formato: (00)0000-0000    } números fixos
             /\A\+\d{12}\z/                   # Formato: +000000000000    }
         ]
-
-        # Checa se o número de telefone está dentro do vetor fornecido
-        unless allowed_phone_formats.any? { |format| phone =~ format }
-            errors.add(:phone, "número de telefone fora do formato permitido")
-        end
-    end
-
-    def cpf_format
-        # Define um vetor de formatos de CPF permitidos
-        allowed_cpf_formats = [
-            /\A\d{3}\.\d{3}\.\d{3}\-\d{2}\z/, # Formato: 000.000.000-00     
-            /\A\d{11}\z/                      # Formato: 00000000000
-        ]
-
-        # Checa se o cpf fornecido está dentro do vetor fornecido
-        unless allowed_cpf_formats.any? { |format| cpf =~ format }
-            errors.add(:cpf, " fora do formato permitido") 
-        end 
-    end
-
-    def cpf_digits
-        #Checa se os últimos 2 digitos são validos diante do algoritmo do cpf
-        cpf_cleanString = cpf.gsub(/\D/,'')
-        cpf_int = cpf_cleanString.to_i 
-        
-        soma = 0
-        for i in (10).downto(2)
-          temp = ((cpf_int/10**i)%10) * i
-          soma = soma + temp
-        end
-        
-        soma2 = 0
-        for j in (11).downto(2)
-          temp = ((cpf_int/10**(j-1))%10) * j
-          soma2 = soma2 + temp
-        end
-        
-        resto = soma % 11
-        if resto < 2
-            comp = 0
-        else
-            comp = 11 - resto
-        end
-
-        resto2 = soma2 % 11
-        
-        if resto2 < 2
-            comp2 = 0
-        else
-            comp2 = 11 - resto2 
-        end
-
-        cpf_digito_um = (cpf_int/10)%10
-        cpf_digito_dois = cpf_int%10
-
-        if ((comp != cpf_digito_um or comp2 != cpf_digito_dois) or all_digits_same?(cpf))
-            errors.add(:cpf, "número de cpf invalido")
-        end
-    end
-
-    
-
-
-    private
-
-    def ajustar_cpf
-        original = self.cpf
-        new_cpf = cpf.gsub(/\D/,'')
-        self.cpf = cpf.gsub(/\D/,'')
-    end
-
-    def uniqueness_cpf
-        if self.cpf_changed? && User.exists?(cpf: self.cpf)
-            errors.add(:cpf, "o cpf digitado já foi utilizado")
-        end
-    end
-
-
-    def ajustar_phone
-        self.phone = self.phone.gsub(/\D/,'')
-        if((self.phone.length != 12) && (self.phone.length != 13))
-            self.phone = "+55" + self.phone
-        else
-            self.phone = "+" + self.phone
-        end
+        valid_format = allowed_phone_formats.any? { |format| format.match?(phone) }
+        return valid_format
     end
 
     def all_digits_same?(input_string)
         # Use a regular expression to check if all 11 digits are the same
         return input_string.match(/\A(\d)\1{10}\z/) != nil
-      end
+    end
         
 
 end
